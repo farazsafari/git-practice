bc7caa77e191296f5630523caa680a2492868533
Faraz Safari <farazsafari9@gmail.com>
2024-02-21 16:51:16 +0100

    refactor register code

--- func.js
+++ func.js
@@ -1,6 +1,10 @@
 const isLogin = (username) => {
   return true;
 };
-const register = (user) =>{
-    return true
-}
+const register = (user) => {
+  if (user) {
+    return true;
+  } else {
+    return false;
+  }
+};
