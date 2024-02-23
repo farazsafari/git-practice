209f609d4d9b613bed1209f83fd1a099053ab9af
Faraz Safari <farazsafari9@gmail.com>
2024-02-23 12:33:14 +0100

    add hi function in app.js

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ let num2 = +prompt("please enter number 2:");
 let sum = num1 + num2;
 alert(sum);
 
+const test = () => {
+  return false;
+};
 
-const test = () =>{
-    return false
-}
+const hi = () => {
+  return true;
+};
