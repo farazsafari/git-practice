900da3210d75eb83d59279b3a9d3c698fe52495f
farazsafari <60386925+farazsafari@users.noreply.github.com>
2024-02-23 13:03:56 +0100

    Update func.js

--- func.js
+++ func.js
@@ -1,17 +1,1 @@
-const isLogin = (username) => {
-  if (username === "faraz"){
-    return true
-  }else{
-    return false
-  }
-};
-const register = (user) => {
-  if (user) {
-    return true;
-  } else {
-    return false;
-  }
-};
 
-const num3 = 59
-const num = 12
